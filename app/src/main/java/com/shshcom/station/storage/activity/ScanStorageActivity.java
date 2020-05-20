f77eca534235b59083985836cfe90e95c66d0488
zero-li <zero.li.zhh@gmail.com>
2020-05-20 17:49:28 +0800

    ScanStorageActivity 拍照入库 UI布局

--- app/src/main/java/com/shshcom/station/storage/activity/ScanStorageActivity.java
+++ app/src/main/java/com/shshcom/station/storage/activity/ScanStorageActivity.java
@@ -3,6 +3,8 @@ package com.shshcom.station.storage.activity;
 import android.app.Activity;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -22,9 +24,11 @@ import java.util.EnumSet;
  * author: zhhli
  * 2020/5/18
  */
-public class ScanStorageActivity extends CaptureActivity {
+public class ScanStorageActivity extends CaptureActivity implements View.OnClickListener {
 
-    boolean isContinuousScan = true;
+    private TextView tv_pickup_code;
+    private TextView tv_last_code_info;
+    private TextView tv_total_number;
 
     private CaptureHelper helper;
     private Camera camera;
@@ -43,24 +47,41 @@ public class ScanStorageActivity extends CaptureActivity {
 
 
     @Override
+    public int getViewfinderViewId() {
+        return 0;
+    }
+
+    @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         activity = this;
 
-        helper = getCaptureHelper();
+        initCapture();
+        initView();
 
+    }
+
+    private void initView(){
+        tv_pickup_code = findViewById(R.id.tv_pickup_code);
+        tv_last_code_info = findViewById(R.id.tv_last_code_info);
+        tv_total_number = findViewById(R.id.tv_total_number);
+
+
+        findViewById(R.id.iv_pickup_code_modify).setOnClickListener(this);
+    }
+
+    private void initCapture(){
+        helper = getCaptureHelper();
         helper.playBeep(true)//播放音效
                 .vibrate(true)//震动
+                .fullScreenScan(true)
                 .supportVerticalCode(true)//支持扫垂直条码，建议有此需求时才使用。
                 .decodeFormats(EnumSet.of(BarcodeFormat.CODE_128))//设置只识别二维码会提升速度
                 .frontLightMode(FrontLightMode.AUTO)//设置闪光灯模式
                 .tooDarkLux(45f)//设置光线太暗时，自动触发开启闪光灯的照度值
                 .brightEnoughLux(100f)//设置光线足够明亮时，自动触发关闭闪光灯的照度值
                 .continuousScan(false);//是否连扫
-
-
-
     }
 
 
@@ -72,8 +93,6 @@ public class ScanStorageActivity extends CaptureActivity {
     @Override
     public boolean onResultCallback(String result) {
 
-
-        ToastUtil.showShort(result);
         takePicture(result);
 
 
@@ -81,20 +100,17 @@ public class ScanStorageActivity extends CaptureActivity {
     }
 
 
-    private void takePicture(String fileName){
+    private void takePicture(String result){
         if(camera== null){
             camera = helper.getCameraManager().getOpenCamera().getCamera();
         }
-
-
-
-
         camera.takePicture(null, null, new Camera.PictureCallback() {
             @Override
             public void onPictureTaken(byte[] data, Camera camera) {
                 camera.stopPreview();
+                ToastUtil.showShort(result);
                 SHCameraHelp shCameraHelp = new SHCameraHelp();
-                String file = shCameraHelp.saveImage(activity, fileName,data);
+                String file = shCameraHelp.saveImage(activity, result,data);
                 LogUtil.d("ScanStorageCase", file);
                 camera.startPreview();
 
@@ -105,4 +121,16 @@ public class ScanStorageActivity extends CaptureActivity {
 
 
 
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.iv_pickup_code_modify:
+
+                break;
+            case R.id.iv_capture:
+                takePicture("123");
+                break;
+        }
+    }
 }
--- app/src/main/java/com/shshcom/station/storage/domain/ScanStorageCase.java
+++ app/src/main/java/com/shshcom/station/storage/domain/ScanStorageCase.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.hardware.Camera;
 import android.util.Log;
 
+import com.mt.bbdj.baseconfig.db.PickupCode;
+import com.mt.bbdj.baseconfig.db.core.GreenDaoUtil;
 import com.mt.bbdj.baseconfig.utls.LogUtil;
 
 /**
@@ -16,14 +18,40 @@ public class ScanStorageCase {
     private Context context;
 
 
+    private PickupCode pickupCode;
 
-    private SHCameraHelp help;
+    private ScanStorageCase(){
 
-    public ScanStorageCase(Context context) {
-        this.context = context;
-        help = new SHCameraHelp();
+    }
+
+    private static class Hold{
+        static ScanStorageCase instance = new ScanStorageCase();
+    }
 
 
+    public void init(Context context) {
+        this.context = context.getApplicationContext();
+    }
+
+    public ScanStorageCase getInstance(){
+        return Hold.instance;
+    }
+
+
+    public PickupCode getCurrentPickCode(){
+        PickupCode code = GreenDaoUtil.getPickCode();
+        if(code == null){
+            code = new PickupCode();
+            code.setCurrentNumber("1000");
+            code.setType(PickupCode.Type.type_1.getDesc());
+        }
+        return pickupCode;
+    }
+
+    public void updatePickCode(PickupCode pickupCode){
+        this.pickupCode = pickupCode;
+
+        GreenDaoUtil.updatePickCode(pickupCode);
     }
 
 
